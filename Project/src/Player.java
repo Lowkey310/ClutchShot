4a164cb789538e78f5e4c4bcf4b1aac4c4851207
tianyu.zhou <tianyu.zhou@mu-c1-17.mullen.pvt.k12.co.us>
2018-10-02 15:04:57 -0600

    2

---
+++ Project/src/Player.java
@@ -0,0 +1,35 @@
+
+public class Player
+	{
+		 private String name;
+		 private double percentage;
+		 
+		 public Player ( String n, double p )
+		 {
+			 name = n;
+			 percentage = p;
+		 }
+
+		public String getName()
+			{
+				return name;
+			}
+
+		public void setName(String name)
+			{
+				this.name = name;
+			}
+
+		public double getPercentage()
+			{
+				return percentage;
+			}
+
+		public void setPercentage(double percentage)
+			{
+				this.percentage = percentage;
+			}
+
+		
+		
+	}
---
+++ Project/src/PlayerStats.java
@@ -0,0 +1,20 @@
+import java.util.ArrayList;
+
+public class PlayerStats
+	{
+
+		public static void main(String[] args)
+			{
+				ArrayList <Player> player = new ArrayList <Player>();
+				player.add(new Player ("Stephen Curry", 48.3));
+				player.add(new Player ("Kyrie Irving", 36.7));
+				player.add(new Player ("Lonzo Ball", 27.3));
+				player.add(new Player ("Andre Iguodala", 23.1));
+				player.add(new Player ("DeAndre Jordan", 11.4 ));
+				
+				
+				
+
+			}
+
+	}
